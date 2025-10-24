994d789a6bf2b79981bafd831eebe860d713d917
Park Dongyeon <66531014+yeon-ee@users.noreply.github.com>
2025-10-24 21:11:49 +0900

    [level 3] Title: 섬 연결하기, Time: 0.10 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,18 +1,24 @@
+from collections import defaultdict
+import heapq
+
 def solution(n, costs):
-    moms = [i for i in range(n)]
-    costs.sort(key= lambda x: x[2])
-    def union(a, b):
-        mom_a = moms[a]
-        mom_b = moms[b]
-        moms[mom_a] = mom_b
-    def find(a):
-        if moms[a] != a:
-            moms[a] = find(moms[a])
-        return moms[a]
-    answer = 0
+    edges = defaultdict(list)
     for edge in costs:
-        x, y, cost = edge
-        if find(x) != find(y):
-            union(x, y)
-            answer += cost
+        edges[edge[0]].append([edge[2], edge[1]])
+        edges[edge[1]].append([edge[2], edge[0]])
+    
+    hq = []
+    heapq.heappush(hq, [0, 0])
+    visited = [False for _ in range(n)]
+    count = 0
+    answer = 0
+    while hq and count < n:
+        cur_cost, cur_node = heapq.heappop(hq)
+        # print(cur_cost, cur_node)
+        if not visited[cur_node]:
+            count += 1
+            visited[cur_node] = True
+            answer += cur_cost
+            for cost, next_node in edges[cur_node]:
+                heapq.heappush(hq, [cost, next_node])
     return answer
